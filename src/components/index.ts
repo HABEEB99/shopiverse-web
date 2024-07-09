4f451fd463aa0941c88f2af2fab015103e214c29
Habeeb <habeebahmadu1@gmail.com>
2024-07-09 23:41:40 +0100

    feat: add user settings

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Header from "@/components/header";
+import Logo from "@/components/logo";
+import { UserSettings } from "@/components/user-settings";
+
+export { Header, Logo, UserSettings };
