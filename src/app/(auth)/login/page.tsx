e5c5fb42cb06dc683972691f238a018ff8dd412d
Habeeb <habeebahmadu1@gmail.com>
2024-07-11 22:35:45 +0100

    feat: add auth pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components";
+
+const LoginPage = () => {
+  return (
+    <div className="w-screen wrapper flex items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/components";
+
+const RegisterPage = () => {
+  return (
+    <div className="w-screen wrapper flex items-center justify-center">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
