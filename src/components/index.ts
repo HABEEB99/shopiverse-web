db17d62018fd9e95d09a09e87c573b5883bea4df
Habeeb <habeebahmadu1@gmail.com>
2024-07-13 19:59:50 +0100

    feat: add register form

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,17 @@
 import Header from "@/components/header";
+import LoginForm from "@/components/forms/login-form";
 import Logo from "@/components/logo";
+import RegisterForm from "@/components/forms/register-form";
 import { UserSettings } from "@/components/user-settings";
 import ThemeProvider from "@/components/theme-provider";
 import ThemeSwitcher from "@/components/theme-switcher";
 
-export { Header, Logo, UserSettings, ThemeProvider, ThemeSwitcher };
+export {
+  Header,
+  LoginForm,
+  Logo,
+  RegisterForm,
+  UserSettings,
+  ThemeProvider,
+  ThemeSwitcher,
+};
