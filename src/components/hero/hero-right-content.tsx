125e82c609f38dc51393d2cfc9c06817b09d9f49
Habeeb <habeebahmadu1@gmail.com>
2024-07-22 22:50:11 +0100

    feat: add hero right content

---
+++ src/components/hero/hero-right-content.tsx
@@ -0,0 +1,9 @@
+const HeroRightContent = () => {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-5 p-3 h-[70vh] w-60 shadow-sm bg-light/25 rounded-md">
+      Adverts
+    </div>
+  );
+};
+
+export default HeroRightContent;
